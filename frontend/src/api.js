f085dcc56feb72a53f7727198b0dd61a2320142a
TeruyaY <terryinwaseda@ruri.waseda.jp>
2026-03-17 00:03:25 +0900

    fixed vite variable name

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // 1. axiosの共通設定（インスタンス）を作成
 const api = axios.create({
   // 環境変数からURLを取得（Viteのルール：import.meta.env）
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000',
   headers: {
     'Content-Type': 'application/json',
   },
